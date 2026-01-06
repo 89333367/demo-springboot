0ad613992a869afc1944d2d20f4a6475f590b804
SunYu <sunyu@uml-tech.com>
2026-01-06 13:45:30 +0800

    增加读取二维码示例

--- src/test/java/sunyu/example/demo/DemoSpringbootApplicationTests.java
+++ src/test/java/sunyu/example/demo/DemoSpringbootApplicationTests.java
@@ -1,5 +1,6 @@
 package sunyu.example.demo;
 
+import cn.hutool.core.io.FileTypeUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.extra.qrcode.QrCodeUtil;
 import cn.hutool.log.Log;
@@ -9,13 +10,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.ttzero.excel.reader.Drawings;
 import org.ttzero.excel.reader.ExcelReader;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
 class DemoSpringbootApplicationTests {
     Log log = LogFactory.get();
+
+    @Test
+    void 读取文件夹里二维码图片() {
+        List<String> list = Arrays.asList("png", "jpg", "jpeg");
+        for (File loopFile : FileUtil.loopFiles("d:/tmp/二维码")) {
+            String type = FileTypeUtil.getType(loopFile);
+            log.info("{}", type);
+            if (list.contains(type)) {
+                try {
+                    String result = QrCodeUtil.decode(loopFile);
+                    log.info("识别内容: " + result);
+                } catch (Exception e) {
+                    log.error("二维码识别失败: " + e.getMessage());
+                }
+            }
+        }
+    }
 
     @Test
     void 读取二维码图片() {
